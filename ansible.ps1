c67bfeffdf652da1cdf98479630537b7b608b3d0
Raymond Gillett <raymondcgillett@gmail.com>
2019-10-18 15:45:10 -0400

    enhancing ansible scripts

---
+++ ansible.ps1
@@ -0,0 +1,14 @@
+﻿$SSH_PATH = "C:\Users\gille\.ssh"
+$WORKSPACE = "D:\workspace\devops\ansible"
+$PLAY = "deploy_webnerdbot"
+$DOCKER_PASSWORD = ""
+clear
+D:
+cd $WORKSPACE
+docker run --rm -i `
+    -v $SSH_PATH/:/tmp/.ssh `
+    -v $WORKSPACE\plays:/ansible/playbook `
+    -v $WORKSPACE\inventory:/etc/ansible `
+    necrobraska/ansible-controller `
+    ansible-playbook -i /etc/ansible/hosts.yml `
+    "$PLAY.yml" --extra-vars "docker_password=$DOCKER_PASSWORD"
